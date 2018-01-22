66b6d261dce17802afe6a1d4b6dff1a7b7fcd89e
nuwanwixx <nuwan.wixx@gmail.com>
2018-01-22 18:29:36 +0530

    face recognition

--- ZirconTechFaceIT/ZirconTechFaceIT/ZirconTechFaceIT/Views/SignUpPage.xaml.cs
+++ ZirconTechFaceIT/ZirconTechFaceIT/ZirconTechFaceIT/Views/SignUpPage.xaml.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Face.Contract;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
